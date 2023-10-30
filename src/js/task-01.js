e7e0379517dc6de53e9e5f819e7fddb1dec3d865
AsyaDev14 <142207257+AsyaDev14@users.noreply.github.com>
2023-10-30 19:09:48 +0200

    task 2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -8,6 +8,7 @@ console.log(`Number of categories: ${arr.length}`)
 
 const log = arr.forEach(li => {
   const titleRef = li.querySelector('h2');
+  console.log("title", titleRef);
   const listRef = li.querySelectorAll('li');
   console.log(`Category: ${titleRef.textContent}
   Elements: ${listRef.length}`);
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulRef = document.querySelector("#ingredients");
+console.log("ulRef", ulRef);
+
+// ingredients.forEach(ingredient => {
+//   const li = document.createElement("li");
+//   li.textContent = ingredient;
+//   li.classList.add("item");
+//   ulRef.append(li);
+//   console.log(ingredient);
+// });
+
+ingredients.map(ingredient => {
+  const li = document.createElement("li");
+    li.textContent = ingredient;
+  li.classList.add("item");
+  ulRef.append(li);
+  console.log(ingredient);
+})
