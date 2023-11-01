d9f5cd1abe1bceba6c862eaf0d27afd1aca6bd52
AsyaDev14 <142207257+AsyaDev14@users.noreply.github.com>
2023-11-01 09:28:58 +0200

    update task 1 && 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,9 +7,6 @@ const ingredients = [
   'Condiments',
 ];
 
-const ulRef = document.querySelector("#ingredients");
-console.log("ulRef", ulRef);
-
 // ingredients.forEach(ingredient => {
 //   const li = document.createElement("li");
 //   li.textContent = ingredient;
@@ -18,10 +15,30 @@ console.log("ulRef", ulRef);
 //   console.log(ingredient);
 // });
 
-ingredients.map(ingredient => {
-  const li = document.createElement("li");
-  li.textContent = ingredient;
-  li.classList.add("item");
-  ulRef.append(li);
-  console.log(ingredient);
-})
+// ingredients.map(ingredient => {
+//   const li = document.createElement("li");
+//   li.textContent = ingredient;
+//   li.classList.add("item");
+//   ulRef.append(li);
+//   console.log(ingredient);
+// })
+
+function ingredientTemplate(ingredient) {
+  const item = document.createElement("li");
+  item.textContent = `${ingredient}`;
+  item.classList.add("item");
+  return item;
+};
+
+function loopTemplate(ingredients) {
+  return ingredients.map(ingredientTemplate)
+};
+
+function renderIngridient(ingredients) {
+  const ulRef = document.querySelector("#ingredients");
+console.log("ulRef", ulRef);
+  const list = loopTemplate(ingredients)
+  ulRef.append(...list);
+}
+
+renderIngridient(ingredients)
